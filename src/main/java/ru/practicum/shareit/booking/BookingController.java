78c5a6834d18d9b6e4cf7aac902d798622325b42
SergeyPrisyazhnyuk <130796016+SergeyPrisyazhnyuk@users.noreply.github.com>
2023-12-13 20:25:29 +0300

    feat : itemRequest Iter 7 added more tests

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -59,8 +59,8 @@ public class BookingController {
     @GetMapping("/owner")
     public List<BookingDtoReturn> getAllByOwner(@RequestHeader(User_ID) Long ownerId,
                                          @RequestParam(name = "state", defaultValue = "ALL") String state,
-                                                @RequestParam(name = "from", defaultValue = "0") @Min(0) Integer from,
-                                                @RequestParam(name = "size", defaultValue = "10") @Min(1) Integer size
+                                                @RequestParam(name = "from", defaultValue = "0") /*@Min(0)*/ Integer from,
+                                                @RequestParam(name = "size", defaultValue = "10") /*@Min(1)*/ Integer size
     ) {
         log.info("Invoke getAllByOwner method with userId = {} and state = {} and from = {} and size = {} ", ownerId, state, from, size);
 
