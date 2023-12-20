f7d81a345ec5ad88d8f76bef488fea7592d7ed06
SergeyPrisyazhnyuk <130796016+SergeyPrisyazhnyuk@users.noreply.github.com>
2023-12-21 00:46:21 +0300

    feat : added docker Iter 1

--- gateway/src/main/java/ru/practicum/shareit/exception/WrongStateException.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class WrongStateException extends RuntimeException {
-
-    public WrongStateException(String message) {
-        super(message);
-    }
-
-}
